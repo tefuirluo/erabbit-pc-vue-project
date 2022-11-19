06e4705477b5c19481e319b04b95d6de480e562d
tefuirluo <tefuirluo@163.com>
2022-11-19 16:29:19 +0800

    首页主体-左侧分类-结构渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="container">
-    HomePage
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!--左侧分类-->
+        <home-category />
+        <!--轮播图-->
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
+import HomeCategory from '@/views/home/components/home-category'
 export default {
-  name: 'HomePage'
+  name: 'PageHome',
+  components: { HomeCategory }
 }
 </script>
-
 <style scoped lang="less">
-.container{
-  .hoverShadow();
-  color: @xtxColor;
-}
+
 </style>
