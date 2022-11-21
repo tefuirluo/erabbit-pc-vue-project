98d87065663b99fdad914a2c437ababcf865612c
tefuirluo <tefuirluo@163.com>
2022-11-21 19:15:28 +0800

    首页主体-新鲜好物

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,24 +9,18 @@
       </div>
     </div>
     <!--新鲜好物-->
-    <home-panel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
-      <template #right>
-        <XtxMore path="/" />
-      </template>
-      <!--面板内容-->
-    </home-panel>
+    <home-new />
     <!--人气推荐-->
   </div>
 </template>
 <script>
 import HomeCategory from '@/views/home/components/home-category'
 import HomeBanner from '@/views/home/components/home-banner'
-import HomePanel from '@/views/home/components/HomePanel'
-import XtxMore from '@/components/library/xtx-more'
+import homeNew from '@/views/home/components/home-new'
 
 export default {
   name: 'PageHome',
-  components: { XtxMore, HomeBanner, HomeCategory, HomePanel }
+  components: { HomeBanner, HomeCategory, homeNew }
 }
 </script>
 <style scoped lang="less">
