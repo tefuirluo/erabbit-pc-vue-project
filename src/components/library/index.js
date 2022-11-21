569b7d2cb0a777ef257d4a71bcfa88253780240a
tefuirluo <tefuirluo@163.com>
2022-11-21 18:45:57 +0800

    首页主体-面板封装

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -5,6 +5,7 @@
 
 import XtxSkeleton from '@/components/library/xtx-skeleton'
 import XtxCarousel from '@/components/library/xtx-carousel'
+import XtxMore from '@/components/library/xtx-more'
 
 export default {
   install (app) {
@@ -12,5 +13,6 @@ export default {
     // 如果要挂载原型 app.config.globalProperties 方式
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
---
+++ src/components/library/xtx-more.vue
@@ -0,0 +1,42 @@
+<template>
+  <RouterLink to="path" class="xtx-more">
+    <span>查看全部</span>
+    <i class="iconfont icon-angle-right"></i>
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  name: 'xtx-more',
+  props: {
+    path: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.xtx-more {
+  margin-top: 2px;
+  span {
+    font-size: 16px;
+    vertical-align: middle;
+    margin-right: 4px;
+    color: #999999;
+  }
+  i {
+    font-sise: 14px;
+    vertical-align: middle;
+    position: relative;
+    top: 2px;
+    color: #cccccc;
+  }
+  &:hover {
+    span, i {
+      color: @xtxColor;
+    }
+  }
+}
+</style>
