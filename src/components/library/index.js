565a3771042d6331889dc0484e0dcc71d1b49474
tefuirluo <tefuirluo@163.com>
2022-11-22 16:31:10 +0800

    首页主体-图片懒加载

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -6,6 +6,7 @@
 import XtxSkeleton from '@/components/library/xtx-skeleton'
 import XtxCarousel from '@/components/library/xtx-carousel'
 import XtxMore from '@/components/library/xtx-more'
+import defaultImg from '@/assets/images/200.png'
 
 export default {
   install (app) {
@@ -14,5 +15,32 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    // 定义指令
+    defineDirective(app)
   }
+}
+// 定义指令
+const defineDirective = (app) => {
+  // 1. 图片懒加载指令
+  // 原理:先存储图片不能在 src 上, 当图片进入可视区, 将你存储图片地址设置给图片元素即可
+  app.directive('lazy', {
+    mounted (el, binging) {
+      const observe = new IntersectionObserver(([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          // 停止观察
+          observe.unobserve(el)
+          // 把指令的值设置给 el 的 src 属性 -> binging.value 就是指令的值
+          // 处理图片加载失败
+          el.onerror = () => {
+            el.src = defaultImg
+          }
+          el.src = binging.value
+        }
+      }, {
+        threshold: 0
+      })
+      // 开启观察
+      observe.observe(el)
+    }
+  })
 }
